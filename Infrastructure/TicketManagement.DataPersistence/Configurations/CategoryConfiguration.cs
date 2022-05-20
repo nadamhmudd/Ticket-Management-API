226f2d2e85624246881ea9aeccf6d426cc415ac9
Nada Mahmoud <nada.elroo799@gmail.com>
2022-05-21 00:28:59 +0200

    add fluent Configuration

---
+++ Infrastructure/TicketManagement.DataPersistence/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace TicketManagement.DataPersistence
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(50)
+                ;
+        }
+    }
+}
---
+++ Infrastructure/TicketManagement.DataPersistence/Configurations/EventConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace TicketManagement.DataPersistence
+{
+    public class EventConfiguration : IEntityTypeConfiguration<Event>
+    {
+        public void Configure(EntityTypeBuilder<Event> builder)
+        {
+            builder.Property(e => e.Name)
+                .IsRequired()
+                .HasMaxLength(50)
+                ;
+        }
+    }
+}
