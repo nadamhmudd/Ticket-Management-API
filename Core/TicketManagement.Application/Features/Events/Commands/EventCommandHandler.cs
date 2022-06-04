7f3d52ff21d26bbc1e36bd519d184ddc32553c12
Nada Mahmoud <nada.elroo799@gmail.com>
2022-06-04 02:37:33 +0200

    send email when event created

--- Core/TicketManagement.Application/Features/Events/Commands/EventCommandHandler.cs
+++ Core/TicketManagement.Application/Features/Events/Commands/EventCommandHandler.cs
@@ -1,4 +1,6 @@
-﻿namespace TicketManagement.Application.Features.Events.Commands
+﻿using TicketManagement.Application.Models.Mail;
+
+namespace TicketManagement.Application.Features.Events.Commands
 {
     public class EventCommandHandler :
                     IRequestHandler<CreateEventCommand, EventDto>,
@@ -8,48 +10,56 @@
         #region Props / Vars
         private readonly IMapper _mapper;
         private readonly IEventRepository _eventRepo; 
+        private readonly IEmailService _emailService;
         private readonly ILogger<EventCommandHandler> _logger;
         #endregion
 
         #region Constructor(s)
-        public EventCommandHandler(IMapper mapper, IEventRepository eventRepo, ILogger<EventCommandHandler> logger)
+        public EventCommandHandler(IMapper mapper, IEventRepository eventRepo, ILogger<EventCommandHandler> logger, IEmailService emailService)
         {
             _mapper = mapper;
             _eventRepo = eventRepo;
             _logger = logger;
+            _emailService = emailService;
         }
         #endregion
 
         #region Create new Event
         public async Task<EventDto> Handle(CreateEventCommand request, CancellationToken cancellationToken)
         {
+            #region Validation
+            var validator = new CreateEventCommandValidator(_eventRepo);
+
+            var validationResult = await validator.ValidateAsync(request);
+
+            if (validationResult.Errors.Any())
+            {
+                throw new Exceptions.ValidationException(validationResult);
+            }
+            #endregion
+
+            var @event = _mapper.Map<Event>(request);
+
+            @event = await _eventRepo.AddAsync(@event);
+
+            #region Send an email
             try
             {
-                #region Validation
-                var validator = new CreateEventCommandValidator(_eventRepo);
-
-                var validationResult = await validator.ValidateAsync(request);
-
-                if (validationResult.Errors.Any())
+                await _emailService.SendEmail(new Email()
                 {
-                    throw new Exceptions.ValidationException(validationResult);
-                }
-                #endregion
-
-                var @event = _mapper.Map<Event>(request);
-
-                @event = await _eventRepo.AddAsync(@event);
-
-                return _mapper.Map<EventDto>(@event);
+                    To = "nada.mhmudd@gmail.com",
+                    Body = $"A new event was created: {request}",
+                    Subject = "A new event was created"
+                });
             }
             catch (Exception ex)
             {
-                if (ex.GetType() != typeof(Exceptions.ValidationException))
-                    _logger.LogError(ex.Message);
+                //this shouldn't stop the API from doing else so this can be logged
+                _logger.LogError($"Mailing about event {@event.Id} failed due to an error with the mail service: {ex.Message}");
+            } 
+            #endregion
 
-                throw;
-            }
-           
+            return _mapper.Map<EventDto>(@event);
         }
         #endregion
 
