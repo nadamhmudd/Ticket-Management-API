17012b99e6e4045efa1d092a039d457c4c9690ea
Nada Mahmoud <nada.elroo799@gmail.com>
2022-06-05 06:16:38 +0200

    update order entity

--- Core/TicketManagement.Domain/Entities/Order.cs
+++ Core/TicketManagement.Domain/Entities/Order.cs
@@ -3,9 +3,15 @@
     public class Order : AuditableEntity
     {
         public Guid Id { get; set; }
-        public Guid UserId { get; set; }
+        public int TicktCounts { get; set; }
         public double OrderTotal { get; set; }
-        public DateTime OrderPlaced { get; set; }
         public bool OrderPaid { get; set; }
+        public DateTime OrderPlaced { get; set; }
+
+        //one to many relationship
+        public Guid UserId { get; set; } //FK
+
+        public Guid EventId { get; set; } //FK
+        public Event Event { get; set; } //navigation property
     }
 }
