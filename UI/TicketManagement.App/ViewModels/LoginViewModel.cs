241f6170aa11d23c2a4c93ee631abf6b0b3d2ea8
Nada Mahmoud <nada.elroo799@gmail.com>
2022-06-05 08:44:07 +0200

    delete blazor UI

--- UI/TicketManagement.App/ViewModels/LoginViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace 
-    TicketManagement.App.ViewModels
-{
-    public class LoginViewModel
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
